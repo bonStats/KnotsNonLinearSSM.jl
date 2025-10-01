c3abe4326ec8c0fd4989ada10583f2e8fc030aaa
Joshua Bon <joshuajbon@gmail.com>
2025-10-02 08:48:50 +0930

    Plots for 7.3 added

--- example/results/plot-ssm-test.r
+++ example/results/plot-ssm-test.r
@@ -7,23 +7,23 @@ library(patchwork)
 library(gt)
 library(matrixStats)
 
-results5 <- read_csv("github/KnotsNonLinearSSM.jl/example/results/ssm-test-d5.csv")
-
-
-gg_outer <- results |> pivot_longer(cols = everything()) |>
- ggplot() + geom_boxplot(aes(x = name, y = value)) +
-  theme_bw() +
-  xlab("Particle Filter") +
-  ylab(TeX("$\\log\\hat{\\gamma}^N_n(1)$"))
-
-
-gg_inner <- results |> pivot_longer(cols = everything()) |> filter(value > -250) |>
-  ggplot() + geom_boxplot(aes(x = name, y = value)) +
-  theme_bw() +
-  xlab("") +
-  ylab("")
-
-gg_outer + inset_element(gg_inner, left = 0.55, bottom = 0.25, right = 0.95, top = 0.75)
+# results5 <- read_csv("github/KnotsNonLinearSSM.jl/example/results/ssm-test-d5.csv")
+# 
+# 
+# gg_outer <- results |> pivot_longer(cols = everything()) |>
+#  ggplot() + geom_boxplot(aes(x = name, y = value)) +
+#   theme_bw() +
+#   xlab("Particle Filter") +
+#   ylab(TeX("$\\log\\hat{\\gamma}^N_n(1)$"))
+# 
+# 
+# gg_inner <- results |> pivot_longer(cols = everything()) |> filter(value > -250) |>
+#   ggplot() + geom_boxplot(aes(x = name, y = value)) +
+#   theme_bw() +
+#   xlab("") +
+#   ylab("")
+# 
+# gg_outer + inset_element(gg_inner, left = 0.55, bottom = 0.25, right = 0.95, top = 0.75)
 
 
 
@@ -41,6 +41,41 @@ results1 <- read_csv("github/KnotsNonLinearSSM.jl/example/results/ssm-test-d1.cs
 results <- bind_rows(results1,results2,results3,results4,results5) |> 
   pivot_longer(cols = -dim) |> 
   group_by(dim,name)
+
+
+results_mean <- results |>
+  filter(name == "KPF") |>
+  group_by(dim) |>
+  summarise(log_mean = logSumExp(value) - log(n()))
+
+results2 <- results |> 
+  left_join(results_mean) |>
+  mutate(normalised_value = value - log_mean) |>
+  mutate(dim_name = paste0('d = ',dim)) |>
+  mutate(name = ifelse(name == "KPF", "TKPF", name))
+
+plot123 <- results2 |> filter(dim <= 3) |> ggplot() +
+  geom_boxplot(aes(x = name, y = normalised_value), outliers = FALSE) +
+  facet_grid(rows = vars(dim_name)) + 
+  ylab(expression(log*hat(gamma)[][n]^{N}*(1))) +
+  xlab("") +
+  theme_bw() + coord_flip()
+
+plot4 <- results2 |> filter(dim == 4) |> ggplot() +
+  geom_boxplot(aes(x = name, y = normalised_value), outliers = FALSE) +
+  facet_grid(rows = vars(dim_name), scales = "free") + 
+  ylab("") + 
+  xlab("") +
+  theme_bw() + coord_flip()
+
+plot5 <- results2 |> filter(dim == 5) |> ggplot() +
+  geom_boxplot(aes(x = name, y = normalised_value), outliers = FALSE) +
+  facet_grid(rows = vars(dim_name), scales = "free") + 
+  ylab(expression(log*hat(gamma)[][n]^{N}*(1))) + 
+  xlab("") +
+  theme_bw() + coord_flip()
+
+plot123 + (plot4 / plot5) # 10 * 3
 
  
 results |> 
